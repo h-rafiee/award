e7111279278f8023aac764832e6c0be3d628d89f
Hossein Raffiee <h.rafiee91@gmail.com>
2020-12-20 02:33:58 +0330

    update winner table migration add event link and unique

--- database/migrations/2020_12_19_220604_create_winners_table.php
+++ database/migrations/2020_12_19_220604_create_winners_table.php
@@ -15,10 +15,22 @@ class CreateWinnersTable extends Migration
     {
         Schema::create('winners', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('event_id');
+            $table->foreign('event_id', 'fk_winners_event_id')
+                ->on('events')
+                ->references('id');
             $table->unsignedBigInteger('mobile_id');
+            $table->foreign('mobile_id', 'fk_winners_mobile_id')
+                ->on('mobiles')
+                ->references('id');
             $table->unsignedBigInteger('event_code_id');
+            $table->foreign('event_code_id', 'fk_winners_event_code_id')
+                ->on('event_codes')
+                ->references('id');
             $table->boolean('is_win')->default(true);
             $table->timestamps();
+
+            $table->unique(['event_id', 'mobile_id', 'event_code_id'], 'unique_winner_columns');
         });
     }
 
