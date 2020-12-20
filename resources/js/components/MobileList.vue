118075bb7193021f5f48019ee0f4511e315e5f64
Hossein Raffiee <h.rafiee91@gmail.com>
2020-12-21 03:20:44 +0330

    add MobileList vue

--- resources/js/components/MobileList.vue
+++ resources/js/components/MobileList.vue
@@ -1,11 +1,43 @@
 <template>
     <div>
         <h1>MobileList</h1>
+        <hr>
+        <table-component
+            :columns="columns"
+            :data="data"
+            url-prefix="/mobiles/"
+            :removeable="false"
+            :editable="false"
+            :watchable="false"
+        />
     </div>
 </template>
 
 <script>
+import axios from "axios";
+import TableComponent from '../objects/Table.vue';
+
 export default {
-    
+    data () {
+        return {
+            columns: [
+                "mobile",
+                "created_at"
+            ],
+            data: [],
+            paginate: [],
+            currentPage: 1,
+        }
+    },
+    components: {
+        TableComponent,
+    },
+    created() {
+        axios.get('/api/mobiles').then(res => {
+            this.data = res.data.data;
+            this.paginate = res.data.links;
+            this.currentPage = res.data.current_page;
+        });
+    },
 }
 </script>
