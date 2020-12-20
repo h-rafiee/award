818cc8a3015748117a5ce5eeccc99b0fda9bf1a2
Hossein Raffiee <h.rafiee91@gmail.com>
2020-12-21 03:20:59 +0330

    add WinnerList vue

--- resources/js/components/WinnerList.vue
+++ resources/js/components/WinnerList.vue
@@ -1,11 +1,48 @@
 <template>
     <div>
         <h1>WinnerList</h1>
+        <hr />
+        <table-component
+            :columns="columns"
+            :data="data"
+            url-prefix="/winners/"
+            :removeable="false"
+            :editable="false"
+            :watchable="false"
+        />
     </div>
 </template>
 
 <script>
+import moduleWinner from "../store/winner/moduleWinner";
+import TableComponent from "../objects/Table";
+
 export default {
-    
-}
+    data () {
+        return {
+            columns: [
+                'mobile',
+                'event',
+                'code',
+                'win',
+                'created_at',
+            ]
+        }
+    },
+    components : {
+        TableComponent,
+    },
+    computed: {
+        data () {
+            return this.$store.state.moduleWinner.winners
+        }
+    },
+    created() {
+        if (!moduleWinner.isRegistered) {
+            this.$store.registerModule("moduleWinner", moduleWinner);
+            moduleWinner.isRegistered = true;
+        }
+        this.$store.dispatch("moduleWinner/get");
+    }
+};
 </script>
